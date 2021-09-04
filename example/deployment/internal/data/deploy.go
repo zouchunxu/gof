7143269aa028393e52bb5b6e2e0475dd113862b3
Chunxu Zou <471500439@qq.com>
2021-09-04 23:18:57 +0800

    update

--- example/deployment/internal/data/deploy.go
+++ example/deployment/internal/data/deploy.go
@@ -7,6 +7,9 @@ import (
 	v1 "k8s.io/api/apps/v1"
 	cv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	appsv1 "k8s.io/client-go/applyconfigurations/apps/v1"
+	corev1 "k8s.io/client-go/applyconfigurations/core/v1"
+	dv1 "k8s.io/client-go/applyconfigurations/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
@@ -21,7 +24,7 @@ func (d deployRepo) ListDeploy(page, pageSize uint32) ([]*biz.Deploy, error) {
 	if err != nil {
 		return nil, err
 	}
-	l, err := clientset.AppsV1().Deployments("").List(context.Background(), metav1.ListOptions{})
+	l, err := clientset.AppsV1().Deployments("default").List(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		return nil, err
 	}
@@ -42,7 +45,7 @@ func (d deployRepo) CreateDeploy(deploy *biz.Deploy) error {
 		d.app.Log.Errorf("get err: %+v", err)
 		return err
 	}
-	var replicas int32 = 2
+	var replicas int32 = 1
 	ds := &v1.Deployment{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "Deployment",
@@ -134,7 +137,40 @@ func (d deployRepo) CreateDeploy(deploy *biz.Deploy) error {
 }
 
 func (d deployRepo) UpdateDeploy(id uint32, deploy *biz.Deploy) error {
-	panic("implement me")
+	k8s, err := getK8sConfig()
+	if err != nil {
+		return err
+	}
+	kind := "Deployment"
+	v := "apps/v1"
+	var cfg = &appsv1.DeploymentApplyConfiguration{
+		TypeMetaApplyConfiguration: dv1.TypeMetaApplyConfiguration{
+			Kind:       &kind,
+			APIVersion: &v,
+		},
+		ObjectMetaApplyConfiguration: &dv1.ObjectMetaApplyConfiguration{
+			Name: &deploy.Name,
+		},
+		Spec: &appsv1.DeploymentSpecApplyConfiguration{
+			Selector: &dv1.LabelSelectorApplyConfiguration{
+				MatchLabels: map[string]string{
+					"app": deploy.Name,
+				},
+			},
+			Template: &corev1.PodTemplateSpecApplyConfiguration{
+				Spec: &corev1.PodSpecApplyConfiguration{
+					Containers: []corev1.ContainerApplyConfiguration{
+						{
+							Name:  &deploy.Name,
+							Image: &deploy.Image,
+						},
+					},
+				},
+			},
+		},
+	}
+	_, err = k8s.AppsV1().Deployments("default").Apply(context.Background(), cfg, metav1.ApplyOptions{})
+	return err
 }
 
 func (d deployRepo) DeleteDeploy(id uint32) error {
