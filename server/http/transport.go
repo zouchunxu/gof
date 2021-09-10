c4a1808bd06e8223c6c54a64e62983f46d31a3a8
Chunxu Zou <471500439@qq.com>
2021-09-10 23:10:17 +0800

    upd

---
+++ server/http/transport.go
@@ -0,0 +1,84 @@
+package http
+
+import (
+	"context"
+	"github.com/zouchunxu/gof/server"
+	"net/http"
+)
+
+var _ server.Transporter = &Transport{}
+
+// Transport is an HTTP transport.
+type Transport struct {
+	endpoint     string
+	operation    string
+	reqHeader    headerCarrier
+	replyHeader  headerCarrier
+	request      *http.Request
+	pathTemplate string
+}
+
+// Kind returns the transport kind.
+func (tr *Transport) Kind() server.Kind {
+	return server.KindHTTP
+}
+
+// Endpoint returns the transport endpoint.
+func (tr *Transport) Endpoint() string {
+	return tr.endpoint
+}
+
+// Operation returns the transport operation.
+func (tr *Transport) Operation() string {
+	return tr.operation
+}
+
+// Request returns the HTTP request.
+func (tr *Transport) Request() *http.Request {
+	return tr.request
+}
+
+// RequestHeader returns the request header.
+func (tr *Transport) RequestHeader() server.Header {
+	return tr.reqHeader
+}
+
+// ReplyHeader returns the reply header.
+func (tr *Transport) ReplyHeader() server.Header {
+	return tr.replyHeader
+}
+
+// PathTemplate returns the http path template.
+func (tr *Transport) PathTemplate() string {
+	return tr.pathTemplate
+}
+
+// SetOperation sets the transport operation.
+func SetOperation(ctx context.Context, op string) {
+	if tr, ok := server.FromServerContext(ctx); ok {
+		if tr, ok := tr.(*Transport); ok {
+			tr.operation = op
+		}
+	}
+}
+
+type headerCarrier http.Header
+
+// Get returns the value associated with the passed key.
+func (hc headerCarrier) Get(key string) string {
+	return http.Header(hc).Get(key)
+}
+
+// Set stores the key-value pair.
+func (hc headerCarrier) Set(key string, value string) {
+	http.Header(hc).Set(key, value)
+}
+
+// Keys lists the keys stored in this carrier.
+func (hc headerCarrier) Keys() []string {
+	keys := make([]string, 0, len(hc))
+	for k := range http.Header(hc) {
+		keys = append(keys, k)
+	}
+	return keys
+}
