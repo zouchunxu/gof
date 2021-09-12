d9e7a4aaa815abf205ea683f04fd8ed22ebc91d4
Chunxu Zou <471500439@qq.com>
2021-09-12 23:18:58 +0800

    grpc

--- server/grpc/grpc.go
+++ server/grpc/grpc.go
@@ -1,16 +1,21 @@
 package grpc
 
 import (
+	"github.com/zouchunxu/gof/internal/endpoint"
+	"github.com/zouchunxu/gof/internal/host"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"net"
 	"net/url"
+	"sync"
 )
 
 type GrpcServer struct {
 	*grpc.Server
-	addr   string
-	health *health.Server
+	addr     string
+	health   *health.Server
+	once     sync.Once
+	endpoint *url.URL
 }
 
 func NewGrpcServer(addr string) *GrpcServer {
@@ -41,5 +46,21 @@ func (g *GrpcServer) Stop() error {
 // examples:
 //   grpc://127.0.0.1:9000?isSecure=false
 func (g *GrpcServer) Endpoint() (*url.URL, error) {
-	panic("implement me")
+	var err error
+	g.once.Do(func() {
+		if g.endpoint != nil {
+			return
+		}
+		lis, err := net.Listen("tcp", g.addr)
+		if err != nil {
+			return
+		}
+		addr, err := host.Extract(g.addr, lis)
+		if err != nil {
+			lis.Close()
+			return
+		}
+		g.endpoint = endpoint.NewEndpoint("grpc", addr, false)
+	})
+	return g.endpoint, err
 }
