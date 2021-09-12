c3da1c6f6300041351afbbb7a549be1eb540e430
Chunxu Zou <471500439@qq.com>
2021-09-12 23:11:24 +0800

    endpoint

--- server/http/http.go
+++ server/http/http.go
@@ -2,19 +2,27 @@ package http
 
 import (
 	"context"
+	"github.com/zouchunxu/gof/internal/endpoint"
+	"github.com/zouchunxu/gof/internal/host"
+	"net"
 	"net/http"
 	"net/url"
+	"sync"
 )
 
 type HttpServer struct {
-	s   *http.Server
-	mux *http.ServeMux
+	s        *http.Server
+	mux      *http.ServeMux
+	once     sync.Once
+	endpoint *url.URL
+	addr     string
 }
 
 func NewHttpServer(addr string) {
 	h := HttpServer{
-		s:   &http.Server{},
-		mux: http.NewServeMux(),
+		s:    &http.Server{},
+		mux:  http.NewServeMux(),
+		addr: addr,
 	}
 	h.s.Handler = h.mux
 }
@@ -35,5 +43,21 @@ func (h *HttpServer) Stop() error {
 // examples:
 //   grpc://127.0.0.1:9000?isSecure=false
 func (h *HttpServer) Endpoint() (*url.URL, error) {
-	panic("implement me")
+	var err error
+	h.once.Do(func() {
+		if h.endpoint != nil {
+			return
+		}
+		lis, err := net.Listen("tcp", h.addr)
+		if err != nil {
+			return
+		}
+		addr, err := host.Extract(h.addr, lis)
+		if err != nil {
+			lis.Close()
+			return
+		}
+		h.endpoint = endpoint.NewEndpoint("http", addr, false)
+	})
+	return h.endpoint, err
 }
