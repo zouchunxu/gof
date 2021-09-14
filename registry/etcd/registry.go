1e4c832a5814ea4e7cdfcdebbc78a30f9240b3c8
Chunxu Zou <471500439@qq.com>
2021-09-14 23:19:14 +0800

    init etcd registry

---
+++ registry/etcd/registry.go
@@ -0,0 +1,132 @@
+package etcd
+
+import (
+	"context"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"math/rand"
+	"time"
+)
+
+type Registry struct {
+	kv     clientv3.KV
+	client *clientv3.Client
+	lease  clientv3.Lease
+}
+
+//New creates etcd registry
+func New(client *clientv3.Client) *Registry {
+	return &Registry{
+		kv:     clientv3.NewKV(client),
+		client: client,
+	}
+}
+
+func (r *Registry) Registry(ctx context.Context) error {
+	key := ""
+	value := ""
+	if r.lease != nil {
+		r.lease.Close()
+	}
+	r.lease = clientv3.NewLease(r.client)
+	leaseID, err := r.registerWithKV(ctx, key, value)
+	if err != nil {
+		return err
+	}
+	go r.heartBeat(ctx, leaseID, key, value)
+	return nil
+}
+
+func (r *Registry) heartBeat(ctx context.Context, leaseID clientv3.LeaseID, key string, value string) {
+	curLeaseID := leaseID
+	kac, err := r.client.KeepAlive(ctx, leaseID)
+	if err != nil {
+		curLeaseID = 0
+	}
+	rand.Seed(time.Now().Unix())
+	for {
+		if curLeaseID == 0 {
+			var retreat []int
+			for retryCnt := 0; retryCnt < 5; retryCnt++ {
+				if ctx.Err() != nil {
+					return
+				}
+				idChan := make(chan clientv3.LeaseID, 1)
+				errChan := make(chan error, 1)
+				cancelCtx, cancel := context.WithCancel(ctx)
+				go func() {
+					defer cancel()
+					id, err := r.registerWithKV(cancelCtx, key, value)
+					if err != nil {
+						errChan <- err
+					} else {
+						idChan <- id
+					}
+				}()
+				select {
+				case <-time.After(3 * time.Second):
+					cancel()
+					continue
+				case <-errChan:
+					continue
+				case curLeaseID = <-idChan:
+				}
+
+				kac, err = r.client.KeepAlive(ctx, curLeaseID)
+				if err == nil {
+					break
+				}
+				retreat = append(retreat, 1<<retryCnt) // 2的retryCnt次方
+				time.Sleep(time.Duration(retreat[rand.Intn(len(retreat))]) * time.Second)
+			}
+			//err
+			if _, ok := <-kac; !ok {
+				return
+			}
+		}
+		select {
+		case _, ok := <-kac:
+			if !ok {
+				if ctx.Err() != nil {
+					return
+				}
+				curLeaseID = 0
+				continue
+			}
+		case <-ctx.Done():
+		}
+
+	}
+}
+
+//registerWithKV create a new lease, return current leaseID
+func (r *Registry) registerWithKV(ctx context.Context, key, value string) (clientv3.LeaseID, error) {
+	grant, err := r.lease.Grant(ctx, int64(time.Second.Seconds())*15)
+	if err != nil {
+		return 0, err
+	}
+	_, err = r.client.Put(ctx, key, value, clientv3.WithLease(grant.ID))
+	if err != nil {
+		return 0, err
+	}
+	return grant.ID, nil
+}
+
+//// Watch creates a watcher according to the service name.
+//func (r *Registry) Watch(ctx context.Context, name string) (registry.Watcher, error) {
+//	key := fmt.Sprintf("%s/%s", r.opts.namespace, name)
+//	return newWatcher(ctx, key, r.client), nil
+//}
+//
+
+//
+//// Deregister the registration.
+//func (r *Registry) Deregister(ctx context.Context, service *registry.ServiceInstance) error {
+//	defer func() {
+//		if r.lease != nil {
+//			r.lease.Close()
+//		}
+//	}()
+//	key := fmt.Sprintf("%s/%s/%s", r.opts.namespace, service.Name, service.ID)
+//	_, err := r.client.Delete(ctx, key)
+//	return err
+//}
